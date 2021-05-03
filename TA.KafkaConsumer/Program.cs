06cefb6ef9e1f841f268bdde56d20d1d24660671
kainthsourav <souravkainth@outlook.com>
2021-05-03 12:27:01 +0530

    Fix

---
+++ TA.KafkaConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TA.KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
